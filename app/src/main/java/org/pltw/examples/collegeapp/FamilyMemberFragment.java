73a2b7fcc506b7b530f0b80a8024b54a35c7ed1a
alcantara4106 <alcantara4106@mydusd.org>
2017-03-09 11:46:45 -0800

    trying to do #11, it's a mess... like usual
    check out the code in strings.xml, familymemberfragment xml, and
    familymemberfragment java file

--- app/src/main/java/org/pltw/examples/collegeapp/FamilyMemberFragment.java
+++ app/src/main/java/org/pltw/examples/collegeapp/FamilyMemberFragment.java
@@ -14,7 +14,12 @@ import android.widget.TextView;
 public class FamilyMemberFragment extends android.support.v4.app.Fragment {
     TextView mFirstName;
     TextView mLastName; //make an EditText for these too?
+    FamilyMember daddy;
 
+    daddy.setFirstName("John");
+    daddy.setLastName("Johnson");
+
+    private String firstNameThing = (String)findViewById(R.id.firstName);
 
     public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle bundle){
         super.onCreateView(inflater, group, bundle);
