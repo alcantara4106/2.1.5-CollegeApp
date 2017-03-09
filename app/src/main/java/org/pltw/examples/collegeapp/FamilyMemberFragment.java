032a7bf98f17d4952205a4dabcbd75a013a0216a
alcantara4106 <alcantara4106@mydusd.org>
2017-03-09 11:24:46 -0800

    Just finished #7, kinda
    see comment on mLastName for more info

--- app/src/main/java/org/pltw/examples/collegeapp/FamilyMemberFragment.java
+++ app/src/main/java/org/pltw/examples/collegeapp/FamilyMemberFragment.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by alcantara94106 on 3/2/2017.
  */
 
 public class FamilyMemberFragment extends android.support.v4.app.Fragment {
+    TextView mFirstName;
+    TextView mLastName; //make an EditText for these too?
+
+
     public View onCreateView(LayoutInflater inflater, ViewGroup group, Bundle bundle){
         super.onCreateView(inflater, group, bundle);
         View rootView = inflater.inflate(R.layout.fragment_family_member, group, false);
